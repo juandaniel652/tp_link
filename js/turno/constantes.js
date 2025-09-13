c52724a248e667a4a623105b0e8c22b64d0b475e
Tu Nombre <juankycabj05@gmail.com>
2025-09-13 18:46:56 -0300

    constante.js cumple una función de manera modulada

---
+++ js/turno/constantes.js
@@ -0,0 +1,9 @@
+export const T_VALUES = [1, 2, 3, 4, 5, 6];
+export const RANGOS = ["AM", "PM"];
+
+export const NOMBRES_DIAS = {
+    domingo: 'Domingo', lunes: 'Lunes', martes: 'Martes', miercoles: 'Miércoles',
+    jueves: 'Jueves', viernes: 'Viernes', sabado: 'Sábado'
+};
+
+export const DAYS = ['domingo','lunes','martes','miercoles','jueves','viernes','sabado'];
