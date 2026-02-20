09dd9deee0674c309a64bcccb33d1e99b64a3cd5
Juan Daniel <juankycabj05@gmail.com>
2026-02-19 22:45:38 -0300

    token en tecnico

--- js/tecnico/tecnicoApi.js
+++ js/tecnico/tecnicoApi.js
@@ -1,11 +1,26 @@
+import { getToken } from "../conexion_backend/tokenStorage.js";
+
 const API_BASE = "https://agenda-1-zomu.onrender.com/api/v1";
 
 export async function obtenerTecnicosBackend() {
+  const token = getToken();
 
-    const response = await fetch(`${API_BASE}/tecnicos/`);
+  console.log("TOKEN:", token);
 
-    if (!response.ok)
-        throw new Error("Error obteniendo tecnicos");
+  const response = await fetch(`${API_BASE}/tecnicos/`, {
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
 
-    return await response.json();
+  console.log("STATUS:", response.status);
+
+  const data = await response.json();
+
+  console.log("RESPONSE DATA:", data);
+
+  if (!response.ok) throw new Error(data.detail || "Error obteniendo tecnicos");
+
+  return data;
 }
