b5ac4ffc0bb9ecadb0da807d2727fad47bee0bf3
Juan Daniel <juankycabj05@gmail.com>
2026-02-19 22:20:00 -0300

    clienteApi.js con token

--- js/cliente/clienteApi.js
+++ js/cliente/clienteApi.js
@@ -1,11 +1,30 @@
+import { getToken } from "../conexion_backend/tokenStorage.js";
+
 const API_BASE = "https://agenda-1-zomu.onrender.com/api/v1";
 
 export async function obtenerClientesBackend() {
 
-    const response = await fetch(`${API_BASE}/clientes/`);
+    const token = getToken();
+
+    console.log("TOKEN:", token);
+
+    const response = await fetch(`${API_BASE}/clientes/`, {
+
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        }
+
+    });
+
+    console.log("STATUS:", response.status);
+
+    const data = await response.json();
+
+    console.log("RESPONSE DATA:", data);
 
     if (!response.ok)
-        throw new Error("Error obteniendo clientes");
+        throw new Error(data.detail || "Error obteniendo clientes");
 
-    return await response.json();
+    return data;
 }
