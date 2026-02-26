f71fdb0fee64ddd49b35411cdef44b3948e2ee1d
Juan Daniel <juankycabj05@gmail.com>
2026-02-26 11:00:56 -0300

    turno.service validando y no creando por el momento

--- js/src/modules/turnos/service/turnos.service.js
+++ js/src/modules/turnos/service/turnos.service.js
@@ -1,35 +1,27 @@
-import { Turno } from '../model/turno.model.js'
+import { Turno } from '../model/turno.model.js';
 
 export class TurnosService {
-
-  static crearTurno(turnosExistentes, datosNuevoTurno) {
-
-    if (!Array.isArray(turnosExistentes)) {
-      throw new TypeError('turnosExistentes debe ser un array')
-    }
-
-    const nuevoTurno = new Turno(datosNuevoTurno)
-
-    const conflicto = turnosExistentes
-      .map(t => new Turno(t))
-      .some(t => t.solapaCon(nuevoTurno))
-
-    if (conflicto) {
-      throw new Error('Conflicto de horario')
-    }
-
-    return nuevoTurno
-  }
 
   static validarNuevoTurno(turnos, datos) {
 
     const nuevo = new Turno(datos);
-    
-    if (this.hayConflicto(turnos, nuevo)) {
+
+    if (TurnosService.hayConflicto(turnos, nuevo)) {
       throw new Error("Conflicto de horario");
     }
-  
+
     return true;
+  }
+
+  static hayConflicto(turnos, nuevo) {
+
+    return turnos.some(t =>
+      t.tecnicoId === nuevo.tecnicoId &&
+      t.fecha === nuevo.fecha &&
+      nuevo.inicio < t.fin &&
+      nuevo.fin > t.inicio
+    );
+
   }
 
 }
