5a7f49927292d21216f1c9b985c0bb4fe55af22b
Juan Daniel <juankycabj05@gmail.com>
2026-02-27 15:29:37 -0300

    Cambio de importacion correcta

--- js/src/modules/turnos/service/disponibilidad.service.js
+++ js/src/modules/turnos/service/disponibilidad.service.js
@@ -1,4 +1,4 @@
-import { apiRequest } from "../api/apiRequest.js";
+import { apiRequest } from "@/core/api/apiRequest.js";
 
 export async function obtenerDisponibilidad({ tecnicoId, fecha, token }) {
   return apiRequest(
