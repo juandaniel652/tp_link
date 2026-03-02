74b16ae9317e1360dcb40a65de6ef968ecd00523
Juan Daniel <juankycabj05@gmail.com>
2026-03-02 11:14:57 -0300

    main.js con importaciones

--- js/src/app/main.js
+++ js/src/app/main.js
@@ -1,7 +1,14 @@
+import { checkAuth } from "@/core/auth/token.guard.js";
+import { initTurnos } from "@/modules/turnos/index.js";
+import { initClientes } from "@/modules/clientes/index.js";
+import { initTecnicos } from "@/modules/tecnicos/index.js";
+
 document.addEventListener("DOMContentLoaded", async () => {
-  await bootstrapProtectedPage(() => {
-    initTurnos();
-    initClientes();
-    initTecnicos();
-  });
+
+  const isAuthenticated = await checkAuth();
+  if (!isAuthenticated) return;
+
+  initTurnos();
+  initClientes();
+  initTecnicos();
 });
