7b8cbf9fe613718506f557d53576ba72d01eb4fe
Juan Daniel <juankycabj05@gmail.com>
2026-03-02 22:43:01 -0300

    main.js coherente con html

--- js/src/app/main.js
+++ js/src/app/main.js
@@ -5,12 +5,19 @@ import { initTecnicos } from "@/modules/tecnicos/index.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-  const isAuthenticated = await requireAuth();
-  console.log("isAuthenticated:", isAuthenticated);
-
+  const isAuthenticated = requireAuth();
   if (!isAuthenticated) return;
 
-  initTurnos();
-  initClientes();
-  initTecnicos();
+  if (document.querySelector("#turnosContainer")) {
+    initTurnos();
+  }
+
+  if (document.querySelector("#clientesTable")) {
+    initClientes();
+  }
+
+  if (document.querySelector("#formGeneral")) {
+    initTecnicos();
+  }
+
 });
